2c5ccac9ddb5bfd1a066ef88c1cb7b57f79f776e
Shubham Nagre <Shubham.Nagre@Cognizant.com>
2022-06-20 12:41:50 +0530

    added routing pages functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { LoginComponent } from './login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RegisterComponent } from './register/register.component';
+
+// Added Routing Functionality
+const appRoutes: Routes = [
+  { path: '', component: WelcomePageComponent },
+  { path: 'register', component: RegisterComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
