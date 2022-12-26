16870b992e36a10e2db7ac88f2d6d2bab9bae1a3
Shubham Nagre <Shubham.Nagre@Cognizant.com>
2022-12-26 21:57:34 +0530

    Added login api service

---
+++ src/app/service/user-auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { LoginUser } from './../interface/login-user';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthService {
+
+  private REST_SERVICE_URL = environment.restServiceURL;
+
+  constructor(private httpc: HttpClient) { }
+
+	loginUser = (user: LoginUser) => {
+		return this.httpc.post<LoginUser>(`${this.REST_SERVICE_URL}/users/login`, user);
+	}
+}
